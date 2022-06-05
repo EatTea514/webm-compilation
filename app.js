2781cb75a05202b7d86cbfb8a401fb42f4d25b8c
J <hadorean@gmail.com>
2022-06-05 09:52:47 -0700

    webm compilation

---
+++ app.js
@@ -0,0 +1,6 @@
+document.body.addEventListener("click", function (evt) {
+    console.dir(this);
+    //note evt.target can be a nested element, not the body element, resulting in misfires
+    console.log(evt.target);
+    window.location.replace("/webm1/index1.html");
+});
